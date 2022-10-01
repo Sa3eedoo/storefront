eff8a335ca5a373d4f29b7c03d188ae273a2dea0
Mahmoud Saeed <mahmoudsaid246@gmail.com>
2022-10-01 02:50:38 +0200

    Creating and Executing Tasks

--- playground/views.py
+++ playground/views.py
@@ -1,23 +1,7 @@
-from django.core.mail import send_mail, mail_admins, BadHeaderError, EmailMessage
 from django.shortcuts import render
-from templated_mail.mail import BaseEmailMessage
+from .tasks import notify_custmoers
 
 
 def say_hello(request):
-    try:
-        send_mail('subject', 'message', 'from2@admin.com', ['ms@customer.com'])
-        mail_admins('subject', 'messsage', html_message='message')
-
-        message = EmailMessage('subject', 'message',
-                               'from2@admin.com', ['ms@customer.com'])
-        message.attach_file('playground/static/images/panda.jpg')
-        message.send()
-
-        message = BaseEmailMessage(
-            template_name='emails/hello.html',
-            context={'name': 'Mahmoud'}
-        )
-        message.send(['ms@customer.com'])
-    except BadHeaderError:
-        pass
+    notify_custmoers.delay('Hello')
     return render(request, 'hello.html', {'name': 'Saeed'})
